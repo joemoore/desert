9e5909d7f6805234b7ca6cc927c004a69afab4aa
pivotal <pivotal@albion.flood.pivotallabs.com>
2009-08-04 08:42:38 -0700

    jlm: migratiing plugin_schema_info to plugin_schema_migrations now works if plugin_schema_migrations already exists, but is empty.

--- lib/desert/plugin_migrations/2.1/extensions/schema_statements.rb
+++ lib/desert/plugin_migrations/2.1/extensions/schema_statements.rb
@@ -3,7 +3,7 @@ ActiveRecord::ConnectionAdapters::SchemaStatements.module_eval do
     initialize_schema_migrations_table_without_plugins
 
     begin
-      execute "CREATE TABLE #{Desert::PluginMigrations::Migrator.schema_migrations_table_name} (plugin_name #{type_to_sql(:string)}, version #{type_to_sql(:string)})"
+      execute "CREATE TABLE IF NOT EXISTS #{Desert::PluginMigrations::Migrator.schema_migrations_table_name} (plugin_name #{type_to_sql(:string)}, version #{type_to_sql(:string)})"
       plugins_and_versions = select_all("SELECT plugin_name, version from #{Desert::PluginMigrations::Migrator.schema_info_table_name}")
       plugins_and_versions.each do |plugin_data|
         plugin_name, version = plugin_data["plugin_name"], plugin_data["version"]
@@ -17,12 +17,19 @@ ActiveRecord::ConnectionAdapters::SchemaStatements.module_eval do
           Integer(migration_version_stripped) <= Integer(version_from_table_stripped)
         end
         migration_versions.each do |migration_version|
+          version = Integer(migration_version.split("_").first.sub(/^0*/, ''))
           insert_sql = ActiveRecord::Base.send(:sanitize_sql, [
             "INSERT INTO #{Desert::PluginMigrations::Migrator.schema_migrations_table_name}(plugin_name, version) VALUES(?, ?)",
             plugin_name,
-            Integer(migration_version.split("_").first.sub(/^0*/, ''))
+            version
           ])
           execute insert_sql
+          cleanup_sql = ActiveRecord::Base.send(:sanitize_sql, [
+            "DELETE FROM #{Desert::PluginMigrations::Migrator.schema_info_table_name} WHERE plugin_name = ? AND version = ?",
+            plugin_name,
+            version
+          ])
+          execute cleanup_sql
         end
       end
     rescue ActiveRecord::StatementInvalid
